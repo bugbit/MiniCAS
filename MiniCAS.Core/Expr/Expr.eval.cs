0d800b0a06ce47329d216f3277367a6a316d1347
bugbit <ohernandezba@gmail.com>
2021-07-20 19:01:52 +0200

    page index

---
+++ MiniCAS.Core/Expr/Expr.eval.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniCAS.Core.Expr
+{
+    public partial class Expr
+    {
+        public virtual Task<Expr> Eval() => Task.FromResult(this);
+    }
+}
